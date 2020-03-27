676703dde7c93e0bc1fbbf9b6af389bb8e1b7a19
chenkeyu <chenkeyu@qq.com>
2020-03-27 12:56:47 +0800

    #3

--- src/main/java/com/cky/springbootdemo/entity/Message.java
+++ src/main/java/com/cky/springbootdemo/entity/Message.java
@@ -1,5 +1,7 @@
 package com.cky.springbootdemo.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,8 +14,12 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(value = "消息对象")
 public class Message {
+    @ApiModelProperty(notes = "消息ID",name="id",required = true,dataType = "Long",value = "1")
     private Long id;
+    @ApiModelProperty(notes = "正文",name="text",required = true,dataType = "String",value = "正文")
     private String text;
+    @ApiModelProperty(notes = "摘要",name="summary",required = true,dataType = "String",value = "摘要")
     private String summary;
 }
